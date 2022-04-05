6fa2103b43a9bb13f8005df051a01c614cde9080
immois <rmoimv@gmail.com>
2022-04-04 23:16:26 -0500

    Add default export

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
-import Button from './button';
-import createTheme, { ThemeProvider } from './theme';
-
-export { Button, createTheme, ThemeProvider };
+export { default as Button } from './button';
+export { default as createTheme, ThemeProvider } from './theme';
+export { default as createConfig, createDefaultConfig } from './config';
+export { default as createRoot, createDefaultRoot } from './root';
