ce9aaca0b1fcdf442388b52a9a8506ad56b06773
immois <rmoimv@gmail.com>
2022-04-06 11:34:31 -0500

    Change the path of ClassNameProps and remove the mergeTheme function

--- packages/react/src/utils/utils.ts
+++ packages/react/src/utils/utils.ts
@@ -1,4 +1,4 @@
-import { ClassNameProps, ThemeProps } from '../theme/theme.types';
+import { ClassNameProps } from '../config/config.types';
 
 const defineClassNamePrefix = (className?: ClassNameProps) => {
   const classNameHasPrefix = className?.prefix;
@@ -7,91 +7,4 @@ const defineClassNamePrefix = (className?: ClassNameProps) => {
   return classNamePrefix;
 };
 
-const mergeThemes = (defaultTheme: ThemeProps, localTheme: ThemeProps) => {
-  const {
-    colors: defaultColors,
-    fonts: defaultFonts,
-    components: defaultComponents,
-    className: defaultClassName
-  } = defaultTheme;
-  const {
-    colors: localColors,
-    fonts: localFonts,
-    components: localComponents,
-    className: localClassName
-  } = localTheme;
-
-  // theme merged
-  const theme: ThemeProps = {
-    className: {
-      ...defaultClassName,
-      ...localClassName
-    },
-    colors: {
-      primary: {
-        ...defaultColors?.primary,
-        ...localColors?.primary
-      },
-      secondary: {
-        ...defaultColors?.secondary,
-        ...localColors?.secondary
-      },
-      tertiary: {
-        ...defaultColors?.tertiary,
-        ...localColors?.tertiary
-      },
-      danger: {
-        ...defaultColors?.danger,
-        ...localColors?.danger
-      },
-      warning: {
-        ...defaultColors?.warning,
-        ...localColors?.warning
-      },
-      success: {
-        ...defaultColors?.success,
-        ...localColors?.success
-      }
-    },
-    fonts: {
-      family: {
-        ...defaultFonts?.family,
-        ...localFonts?.family
-      },
-      sizes: {
-        ...defaultFonts?.sizes,
-        ...localFonts?.sizes
-      },
-      weight: {
-        ...defaultFonts?.weight,
-        ...localFonts?.weight
-      }
-    },
-    components: {
-      button: {
-        props: {
-          ...defaultComponents?.button?.props,
-          ...localComponents?.button?.props
-        },
-        shortcuts: {
-          ...defaultComponents?.button?.shortcuts,
-          ...localComponents?.button?.shortcuts
-        },
-        custom: {
-          sizes: {
-            ...defaultComponents?.button?.custom?.sizes,
-            ...localComponents?.button?.custom?.sizes
-          }
-        },
-        theme: {
-          ...defaultComponents?.button?.theme,
-          ...localComponents?.button?.theme
-        }
-      }
-    }
-  };
-
-  return theme;
-};
-
-export { mergeThemes, defineClassNamePrefix };
+export { defineClassNamePrefix };
