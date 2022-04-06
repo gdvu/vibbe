27170a4d33d5ee95aeeed680c95c6aed464d9850
immois <rmoimv@gmail.com>
2022-04-06 11:29:50 -0500

    Button import update

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from './button';
+export { default as Button } from './Button';
 export { default as createTheme, ThemeProvider } from './theme';
 export { default as createConfig, createDefaultConfig } from './config';
 export { default as createRoot, createDefaultRoot } from './root';
