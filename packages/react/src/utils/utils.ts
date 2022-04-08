a343fa33a9d6f7200e91eff144903800e2d9535c
immois <rmoimv@gmail.com>
2022-04-08 14:20:11 -0500

    Function for sizes define

--- packages/react/src/utils/utils.ts
+++ packages/react/src/utils/utils.ts
@@ -1,4 +1,9 @@
-import { ClassNameProps } from '../config/config.types';
+import {
+  ClassNameProps,
+  SizesOptionsProps,
+  SizesPropsConfig
+} from '../config/config.types';
+import { SizesProps } from './global.types';
 
 const defineClassNamePrefix = (className?: ClassNameProps) => {
   const classNameHasPrefix = className?.prefix;
@@ -7,4 +12,29 @@ const defineClassNamePrefix = (className?: ClassNameProps) => {
   return classNamePrefix;
 };
 
-export { defineClassNamePrefix };
+const defineSizes = (
+  sizes?: SizesProps,
+  measurements?: SizesPropsConfig
+): SizesOptionsProps | undefined => {
+  if (sizes === 'xsmall') {
+    return measurements?.xsmall;
+  }
+
+  if (sizes === 'small') {
+    return measurements?.small;
+  }
+
+  if (sizes === 'medium') {
+    return measurements?.medium;
+  }
+
+  if (sizes === 'large') {
+    return measurements?.large;
+  }
+
+  if (sizes === 'xlarge') {
+    return measurements?.xlarge;
+  }
+};
+
+export { defineClassNamePrefix, defineSizes };
