bc8889327f31deedafbb3c57b82e6340b59237e3
immois <rmoimv@gmail.com>
2022-05-04 13:41:01 -0500

    Layout: add height types

---
+++ packages/react/src/styles/layout.ts
@@ -0,0 +1,41 @@
+export interface Height {
+  h0?: boolean;
+  h1?: boolean;
+  h2?: boolean;
+  h3?: boolean;
+  h4?: boolean;
+  h5?: boolean;
+  h6?: boolean;
+  h7?: boolean;
+  h8?: boolean;
+  h9?: boolean;
+  h10?: boolean;
+  h11?: boolean;
+  h12?: boolean;
+  h13?: boolean;
+  h14?: boolean;
+  h15?: boolean;
+  h16?: boolean;
+  h17?: boolean;
+  h18?: boolean;
+  h19?: boolean;
+  h20?: boolean;
+  h21?: boolean;
+  h22?: boolean;
+  h23?: boolean;
+  h24?: boolean;
+  h25?: boolean;
+  hView30?: boolean;
+  hView50?: boolean;
+  hViewFull?: boolean;
+  hPorc25?: boolean;
+  hPorc50?: boolean;
+  hPorc75?: boolean;
+  hPorc80?: boolean;
+  hPorc1?: boolean;
+  hPorc2?: boolean;
+  hPorc3?: boolean;
+  hPorcFull?: boolean;
+}
+
+export type Layout = Height;
