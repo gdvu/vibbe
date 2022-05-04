9e196c7dc149f76be71977dbb059f3572b16bfe9
immois <rmoimv@gmail.com>
2022-05-04 00:10:05 -0500

    Borders: add border-radius types

---
+++ packages/react/src/styles/borders.ts
@@ -0,0 +1,10 @@
+export interface BorderRadius {
+  brXsmall?: boolean;
+  brSmall?: boolean;
+  brMedium?: boolean;
+  brLarge?: boolean;
+  brXlarge?: boolean;
+  brRounded?: boolean;
+}
+
+export type Borders = BorderRadius;
