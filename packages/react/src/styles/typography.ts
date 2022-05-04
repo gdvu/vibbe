31934c834725a575e070d0815411cfeaec9b8b57
immois <rmoimv@gmail.com>
2022-05-04 00:32:44 -0500

    Typography: add font-weight types

--- packages/react/src/styles/typography.ts
+++ packages/react/src/styles/typography.ts
@@ -6,4 +6,16 @@ export interface FontSize {
   fsXlarge?: boolean;
 }
 
-export type Typography = FontSize;
+export interface FontWeight {
+  fwThin?: boolean;
+  fwExtraLight?: boolean;
+  fwLight?: boolean;
+  fwRegular?: boolean;
+  fwMedium?: boolean;
+  fwSemiBold?: boolean;
+  fwBold?: boolean;
+  fwExtraBold?: boolean;
+  fwBlack?: boolean;
+}
+
+export type Typography = FontSize & FontWeight;
