a11797a6b61aba0e89efd6807d5a2c6344771a6f
immois <rmoimv@gmail.com>
2022-05-04 00:19:10 -0500

    Borders: add border-width types

--- packages/react/src/styles/borders.ts
+++ packages/react/src/styles/borders.ts
@@ -7,4 +7,36 @@ export interface BorderRadius {
   brRounded?: boolean;
 }
 
-export type Borders = BorderRadius;
+export interface BorderWidth {
+  bw1?: boolean;
+  bw2?: boolean;
+  bw3?: boolean;
+  bw4?: boolean;
+  bw5?: boolean;
+
+  bwt1?: boolean;
+  bwt2?: boolean;
+  bwt3?: boolean;
+  bwt4?: boolean;
+  bwt5?: boolean;
+
+  bwbt?: boolean;
+  bwb2?: boolean;
+  bwb3?: boolean;
+  bwb4?: boolean;
+  bwb5?: boolean;
+
+  bwl1?: boolean;
+  bwl2?: boolean;
+  bwl3?: boolean;
+  bwl4?: boolean;
+  bwl5?: boolean;
+
+  bwr1?: boolean;
+  bwr2?: boolean;
+  bwr3?: boolean;
+  bwr4?: boolean;
+  bwr5?: boolean;
+}
+
+export type Borders = BorderRadius & BorderWidth;
