b383f200e7634c575b5e6924f483d98e878f8af7
immois <rmoimv@gmail.com>
2022-05-14 23:27:52 -0500

    Borders: delete RadiusCSS types

--- packages/react/src/styles/borders.ts
+++ packages/react/src/styles/borders.ts
@@ -84,16 +84,4 @@ export interface BordersCSS {
   borderStyleY?: string;
   borderWidthY?: string;
   borderColorY?: string;
-
-  radius?: string;
-  radiusTop?: string;
-  radiusTopLeft?: string;
-  radiusTopRight?: string;
-  radiusBottom?: string;
-  radiusBottomLeft?: string;
-  radiusBottomRight?: string;
-  radiusLeft?: string;
-  radiusRight?: string;
-  radiusSlash?: string;
-  radiusBackSlash?: string;
 }
