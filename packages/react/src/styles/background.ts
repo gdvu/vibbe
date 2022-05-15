98c83f73e00ffd5d2d2b501daa82c6da01d89b1e
immois <rmoimv@gmail.com>
2022-05-14 23:16:34 -0500

    Add background: styles of styledCSS

--- packages/react/src/styles/background.ts
+++ packages/react/src/styles/background.ts
@@ -6,3 +6,22 @@ export type BackgroundsCSS = {
   linearGradient?: string;
   radialGradient?: string;
 };
+
+export const backgroundCss = (vcs?: BackgroundsCSS) => {
+  return `
+    ${vcs?.backgroundColor ? `background-color: ${vcs?.backgroundColor};` : ''}
+    ${vcs?.background ? `background: ${vcs?.background};` : ''}
+    ${vcs?.backgroundClip ? `background-clip: ${vcs?.backgroundClip};` : ''}
+    ${vcs?.backgroundImage ? `background-image: ${vcs?.backgroundImage};` : ''}
+    ${
+      vcs?.linearGradient
+        ? `background-image: linear-gradient(${vcs?.linearGradient});`
+        : ''
+    }
+    ${
+      vcs?.radialGradient
+        ? `background-image: radial-gradient(${vcs?.radialGradient});`
+        : ''
+    }
+  `;
+};
