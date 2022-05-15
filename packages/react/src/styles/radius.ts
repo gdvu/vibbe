d524763d3fc8822fb1eb19117be8d291223d5965
immois <rmoimv@gmail.com>
2022-05-14 23:25:51 -0500

    Add Radius: RadiusCSS types

---
+++ packages/react/src/styles/radius.ts
@@ -0,0 +1,13 @@
+export interface RadiusCSS {
+  radius?: string;
+  radiusTop?: string;
+  radiusTopLeft?: string;
+  radiusTopRight?: string;
+  radiusBottom?: string;
+  radiusBottomLeft?: string;
+  radiusBottomRight?: string;
+  radiusLeft?: string;
+  radiusRight?: string;
+  radiusSlash?: string;
+  radiusBackSlash?: string;
+}
