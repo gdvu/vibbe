d3fa38d28db6d35ca90d4e229334e774395ad709
immois <rmoimv@gmail.com>
2022-05-15 23:31:56 -0500

    Core: styles of styledCSS and types

--- packages/react/src/styles/core.ts
+++ packages/react/src/styles/core.ts
@@ -1,11 +1,51 @@
-import { BackgroundsCSS } from './background';
-import { BordersCSS } from './borders';
-import { LayoutCSS } from './layout';
-import { SpacingCSS } from './spacing';
-import { TypographyCSS } from './typography';
+import { backgroundCss, BackgroundsCSS } from './background';
+import { borderCss, BordersCSS } from './borders';
+import { layoutCss, LayoutCSS } from './layout';
+import { radiusCss, RadiusCSS } from './radius';
+import { spacingCss, SpacingCSS } from './spacing';
+import { typographyCss, TypographyCSS } from './typography';
 
-export type StyledCSS = SpacingCSS &
+// styled css
+export type StyledCSSBox = SpacingCSS &
   TypographyCSS &
   BordersCSS &
+  RadiusCSS &
   LayoutCSS &
   BackgroundsCSS;
+
+export interface StyledCSSElement {
+  _hover?: StyledCSSBox;
+  _focus?: StyledCSSBox;
+  _active?: StyledCSSBox;
+  _visited?: StyledCSSBox;
+}
+
+export type StylesCSS = StyledCSSBox & StyledCSSElement;
+
+export interface DinamicProp {
+  [key: string]: StylesCSS;
+}
+
+// variant styled css
+export interface StylesVariant {
+  variants?: DinamicProp;
+}
+
+// all styled css
+export type AllStylesCSS = StylesCSS & StylesVariant;
+
+// all types css
+export interface CSS {
+  styled?: AllStylesCSS;
+}
+
+export const styledCss = (vcs?: AllStylesCSS) => {
+  return `
+    ${layoutCss(vcs) ?? ''}
+    ${spacingCss(vcs) ?? ''}
+    ${backgroundCss(vcs) ?? ''}
+    ${borderCss(vcs) ?? ''}
+    ${radiusCss(vcs) ?? ''}
+    ${typographyCss(vcs) ?? ''}
+  `;
+};
