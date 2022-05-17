2f1855a0e82fb775619cd39f5bc6e6bbc844619d
immois <rmoimv@gmail.com>
2022-05-16 23:05:17 -0500

    utils: add function mergeCss

--- packages/react/src/styles/utils.ts
+++ packages/react/src/styles/utils.ts
@@ -1,5 +1,14 @@
+import { AllStylesCSS } from '.';
+
 export const clearCss = (cssIn: string) => {
   const cssInTrim = cssIn.trim();
   const cssOut = cssInTrim.replace(/\s+/g, ' ');
   return cssOut;
+};
+
+export const mergeCss = (cssOne?: AllStylesCSS, cssTwo?: AllStylesCSS) => {
+  return {
+    ...cssOne,
+    ...cssTwo
+  };
 };
