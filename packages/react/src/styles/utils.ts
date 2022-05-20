407c726ab43d0edfcade5a6878690f4afd26a047
immois <rmoimv@gmail.com>
2022-05-19 23:03:52 -0500

    utils: add unionStyled function

--- packages/react/src/styles/utils.ts
+++ packages/react/src/styles/utils.ts
@@ -1,4 +1,5 @@
-import { AllStylesCSS } from '.';
+import { AllStylesCSS, CsxProps, StyledTwo } from '.';
+import { objBoolean } from '../utils';
 
 export const clearCss = (cssIn: string) => {
   const cssInTrim = cssIn.trim();
@@ -25,4 +26,118 @@ export const purgeStyled = (styled: AllStylesCSS) => {
   delete styled._before;
   delete styled._placeholder;
   return styled;
+};
+
+interface OptionsUnionProp {
+  size?: string;
+  variant?: string;
+  disabled?: boolean;
+}
+
+export const unionStyled = (
+  styled: AllStylesCSS,
+  csx: CsxProps | undefined,
+  options: OptionsUnionProp
+): StyledTwo => {
+  const { size, variant, disabled } = options;
+
+  const sizess = styled?.sizes;
+  const variants = styled?.variants;
+
+  const sizeSd = sizess?.[size ?? ''];
+  const variantSd = variants?.[variant ?? ''];
+  const variantSize = sizess?.[size ?? '']?.sizes;
+  const disabledSd = disabled && styled.disabled;
+  const variantDisabled = disabled && variants?.[variant ?? '']?.disabled;
+
+  // hover
+  const hoverSd = styled._hover;
+  const variantHover = variants?.[variant ?? '']?._hover;
+  const cssVariantDisabledHover =
+    disabled && variants?.[variant ?? '']?.disabled?._hover;
+
+  // active
+  const activeSd = styled._active;
+  const variantActive = variants?.[variant ?? '']?._active;
+  const cssVariantDisabledActive =
+    disabled && variants?.[variant ?? '']?.disabled?._active;
+
+  // focus
+  const focusSd = styled._focus;
+  const variantFocus = variants?.[variant ?? '']?._focus;
+  const cssVariantDisabledFocus =
+    disabled && variants?.[variant ?? '']?.disabled?._focus;
+
+  // visited
+  const visitedSd = styled._visited;
+  const variantvisited = variants?.[variant ?? '']?._visited;
+  const cssVariantDisabledvisited =
+    disabled && variants?.[variant ?? '']?.disabled?._visited;
+
+  // placeholder
+  const placeholderSd = styled._placeholder;
+  const variantplaceholder = variants?.[variant ?? '']?._placeholder;
+  const cssVariantDisabledplaceholder =
+    disabled && variants?.[variant ?? '']?.disabled?._placeholder;
+
+  const baseUni = purgeStyled({
+    ...styled,
+    ...sizeSd,
+    ...variantSd,
+    ...variantSize,
+    ...disabledSd,
+    ...variantDisabled,
+    ...csx
+  });
+
+  const hoverUni = purgeStyled({
+    ...hoverSd,
+    ...variantHover,
+    ...cssVariantDisabledHover,
+    ...csx?._hover
+  });
+
+  const activeUni = purgeStyled({
+    ...activeSd,
+    ...variantActive,
+    ...cssVariantDisabledActive,
+    ...csx?._active
+  });
+
+  const focusUni = purgeStyled({
+    ...focusSd,
+    ...variantFocus,
+    ...cssVariantDisabledFocus,
+    ...csx?._focus
+  });
+
+  const visitedUni = purgeStyled({
+    ...visitedSd,
+    ...variantvisited,
+    ...cssVariantDisabledvisited,
+    ...csx?._visited
+  });
+
+  const placeholderUni = purgeStyled({
+    ...placeholderSd,
+    ...variantplaceholder,
+    ...cssVariantDisabledplaceholder,
+    ...csx?._placeholder
+  });
+
+  const base = objBoolean(baseUni) ? undefined : baseUni;
+  const _hover = objBoolean(hoverUni) ? undefined : hoverUni;
+  const _active = objBoolean(activeUni) ? undefined : activeUni;
+  const _focus = objBoolean(focusUni) ? undefined : focusUni;
+  const _visited = objBoolean(visitedUni) ? undefined : visitedUni;
+  const _placeholder = objBoolean(placeholderUni) ? undefined : placeholderUni;
+
+  return {
+    base,
+    _hover,
+    _active,
+    _focus,
+    _visited,
+    _placeholder
+  };
 };
