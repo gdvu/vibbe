e8d1ac5a04cb3ccba28e1462784b898a6f50ef60
immois <rmoimv@gmail.com>
2022-05-19 22:55:35 -0500

    utils: add purgeStyled function

--- packages/react/src/styles/utils.ts
+++ packages/react/src/styles/utils.ts
@@ -12,3 +12,17 @@ export const mergeCss = (cssOne?: AllStylesCSS, cssTwo?: AllStylesCSS) => {
     ...cssTwo
   };
 };
+
+export const purgeStyled = (styled: AllStylesCSS) => {
+  delete styled.disabled;
+  delete styled.variants;
+  delete styled.sizes;
+  delete styled._hover;
+  delete styled._active;
+  delete styled._focus;
+  delete styled._visited;
+  delete styled._after;
+  delete styled._before;
+  delete styled._placeholder;
+  return styled;
+};
